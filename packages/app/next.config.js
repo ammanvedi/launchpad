b20092fb42c4d494292acbc551e09be5e6e7b6b6
ammanvedi <amman.vedi@gmail.com>
2020-11-19 14:45:34 +0000

    add config for env

---
+++ packages/app/next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        AMPLIFY_JWK_URL: process.env.AMPLIFY_JWK_URL,
+        AMPLIFY_JWK_RAW: process.env.AMPLIFY_JWK_RAW,
+        AMPLIFY_ISSUER: process.env.AMPLIFY_ISSUER,
+        AMPLIFY_AUD: process.env.AMPLIFY_AUD,
+        AMPLIFY_CONFIG: process.env.AMPLIFY_CONFIG,
+    }
+};
