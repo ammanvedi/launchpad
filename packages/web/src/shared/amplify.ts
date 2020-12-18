464d1628bedc76a947b03012d5473fecf3fe4524
ammanvedi <amman.vedi@gmail.com>
2020-12-18 17:23:45 +0000

    update the raw values to log themselves

--- packages/web/src/shared/amplify.ts
+++ packages/web/src/shared/amplify.ts
@@ -1,3 +1,26 @@
+import { createLoggerSet } from '../../../api/src/lib/logging/logger';
+
+const log = createLoggerSet('AmplifyCredentials');
+
+log.info(
+    `Amplify Config Raw Values :: ${JSON.stringify(
+        {
+            region: process.env.TF_VAR_aws_region,
+            userPoolId: process.env.TF_VAR_aws_user_pool_id,
+            userPoolClientId: process.env.TF_VAR_aws_user_pool_client_id,
+            userPoolDomain: process.env.TF_VAR_user_pool_domain,
+            signInRedirect: process.env.TF_VAR_sign_in_callback_url,
+            signOutRedirect: process.env.TF_VAR_sign_out_callback_url,
+            cookieDomain: process.env.TF_VAR_auth_cookie_domain,
+            cookiePath: process.env.TF_VAR_auth_cookie_path,
+            cookieExpires: process.env.TF_VAR_auth_cookie_expiry_days,
+            cookieSecure: process.env.TF_VAR_auth_cookie_secure,
+        },
+        null,
+        2,
+    )}`,
+);
+
 export const amplifyConfig = {
     aws_project_region: process.env.TF_VAR_aws_region,
     aws_cognito_region: process.env.TF_VAR_aws_region,
@@ -16,12 +39,12 @@ export const amplifyConfig = {
 export const amplifyAuthConfig = {
     region: process.env.TF_VAR_aws_region,
     userPoolId: process.env.TF_VAR_aws_user_pool_id,
-    userPoolWebClientId: process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
+    userPoolWebClientId: process.env.TF_VAR_aws_user_pool_client_id,
     cookieStorage: {
         domain: process.env.TF_VAR_auth_cookie_domain,
         path: process.env.TF_VAR_auth_cookie_path,
         expires: parseInt(process.env.TF_VAR_auth_cookie_expiry_days || '365'),
-        secure: JSON.parse(process.env.TF_VAR_auth_cookie_secure || ''),
+        secure: JSON.parse(process.env.TF_VAR_auth_cookie_secure || 'true'),
     },
 };
 
